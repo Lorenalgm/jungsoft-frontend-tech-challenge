828fb3222946fac0d13de43a8613082adb7171f6
Lorenalgm <lorena@proesc.com>
2020-06-06 16:54:54 -0300

    Created html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import './App.css';
 
-const GET_PLANS = gql `
+const GET_PLANS = gql`
 {
   listPlans {
     id
@@ -19,12 +19,51 @@ const GET_PLANS = gql `
 function App() {
   const { loading, error, data } = useQuery(GET_PLANS);
 
-  if(error) return <h1>Opa, algo deu errado :(</h1>
-  if(loading) return <h1>Carregando...</h1>
+  if (error) return <h1>Opa, algo deu errado :(</h1>
+  if (loading) return <h1>Carregando...</h1>
 
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div className="title">
+        <h1>Configure o plano que</h1>
+        <h1><b>melhor encaixa na sua rotina</b></h1>
+      </div>
+      <div className="container">
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/18/1a/d5/1e/casteloes.jpg" alt="Food" />
+        <div className="content">
+          <div className="content-title">
+              <h1>Gostou e ainda não é assinante?</h1>
+              <h1>Escolha já um plano semanal!</h1>
+          </div>
+          <div className="plans">
+            <div className="people">
+              <div className="people-title">Receita para quantas pessoas?</div>
+              <div className="people-number">
+                <div>2</div>
+                <div>4</div>
+              </div>
+            </div>
+          </div>
+          <div className="recipes">
+            <div className="recipes-title">Quantas receitas por semana?</div>
+            <div className="recipes-number">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+          </div>
+        </div>
+        <div className="content-submit">
+          <div className="price">
+            <p>Preço do kit por semana</p>
+            <h1>R$69,00</h1>
+          </div>
+          <button>Quero assinar agora!</button>
+        </div>
+      </div>
+
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -38,8 +77,8 @@ function App() {
             </div>
           ))}
         </div>
-      </header>
-    </div>
+      </header> */}
+    </div >
   );
 }
 
