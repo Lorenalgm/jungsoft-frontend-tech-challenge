56e4914483879b6fc9500e758a83d36951121a5d
Lorenalgm <lorena@proesc.com>
2020-06-07 02:00:34 -0300

    Added css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import './App.css';
+import logo from './assets/backgrounds/bg1.jpg';
+import user from './assets/icons/user.svg';
+import clock from './assets/icons/clock.svg';
+import tools from './assets/icons/kitchen-tools.svg';
 
 const GET_PLANS = gql`
 {
@@ -15,77 +19,88 @@ const GET_PLANS = gql`
 
 `
 
-
 function App() {
   const { loading, error, data } = useQuery(GET_PLANS);
-  const [numberOfPeople, setNumberOfPeople] = useState('');
-  const [weeklyRecipes, setWeeklyRecipes] = useState('');
-  const arrayPeople = [2,3,4];    
-  const arrayRecipe = [3,4,5,6];    
+  const [numberOfPeople, setNumberOfPeople] = useState(3);
+  const [weeklyRecipes, setWeeklyRecipes] = useState(3);
+  const arrayPeople = [3, 4];
+  const arrayRecipe = [3, 4, 5];
 
   if (error) return <h1>Opa, algo deu errado :(</h1>
   if (loading) return <h1>Carregando...</h1>
-  
-  function updateWeeklyRecipes(number){
+
+  function updateWeeklyRecipes(number) {
     setWeeklyRecipes(number);
   }
 
-  function updateNumberOfPeople(number){
+  function updateNumberOfPeople(number) {
     setNumberOfPeople(number);
   }
 
   return (
-    <div className="App">      
+    <div className="App">
 
       <div className="title">
         <h1>Configure o plano que</h1>
-        <h1><b>melhor encaixa na sua rotina</b></h1>
+        <h1><span className="teste">melhor encaixa na sua rotina</span></h1>
       </div>
       <div className="container">
-        <img src="https://media-cdn.tripadvisor.com/media/photo-s/18/1a/d5/1e/casteloes.jpg" alt="Food" />
+        <img src={logo} alt="Food" />
         <div className="content">
           <div className="content-title">
-              <h1>Gostou e ainda não é assinante?</h1>
-              <h1>Escolha já um plano semanal!</h1>
+            <h1>Gostou e ainda não é assinante?</h1>
+            <h1>Escolha já um plano semanal!</h1>
           </div>
           <div className="plans">
             <div className="people">
-              <div className="people-title">Receita para quantas pessoas?</div>
+              <div className="people-title">
+                <img src={user} alt={user} />
+                <p>
+                  <span>Receita para</span><br></br>
+                  <span>quantas pessoas?</span>
+                </p>
+              </div>
               <div className="people-number">
                 {
                   arrayPeople.map(people => (
-                    <button onClick={() => updateNumberOfPeople(people)}>{people}</button>
+                    <button className={numberOfPeople === people? 'active': ''} onClick={() => updateNumberOfPeople(people)}>{people}</button>
+                  ))
+                }
+              </div>
+            </div>
+            <div className="recipes">
+              <div className="recipes-title">
+                <img src={tools} alt={tools} />
+                <p>
+                  <span>Quantas receitas</span><br></br>
+                  <span>por semana?</span>
+                </p>
+              </div>
+              <div className="recipes-number">
+                {
+                  arrayRecipe.map(recipe => (
+                    <button className={weeklyRecipes === recipe? 'active': ''} onClick={() => updateWeeklyRecipes(recipe)}>{recipe}</button>
                   ))
                 }
               </div>
             </div>
           </div>
-          <div className="recipes">
-            <div className="recipes-title">Quantas receitas por semana?</div>
-            <div className="recipes-number">
+          <div className="content-submit">
+            <div className="price">
+              <p>PREÇO DO KIT POR SEMANA</p>
+              <span>
                 {
-                  arrayRecipe.map(recipe => (
-                    <button onClick={() => updateWeeklyRecipes(recipe)}>{recipe}</button>
+                  data.listPlans.map(plan => (
+                    plan.weeklyRecipes === weeklyRecipes && plan.numberOfPeople === numberOfPeople &&
+                    <h1 key={plan.id}><span>R$</span> {plan.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h1>
                   ))
                 }
+              </span>
             </div>
+            <button>Quero assinar agora!</button>
           </div>
         </div>
-        <div className="content-submit">
-          <div className="price">
-            <p>Preço do kit por semana</p>
-            <span>
-            {
-                data.listPlans.map(plan => (
-                  plan.weeklyRecipes === weeklyRecipes && plan.numberOfPeople === numberOfPeople &&
-                    <h1 key={plan.id}>price: {plan.price}</h1>
-                ))
-            }
-                </span>
-          </div>
-          <button>Quero assinar agora!</button>
-        </div>
-      </div>     
+      </div>
     </div >
   );
 }
